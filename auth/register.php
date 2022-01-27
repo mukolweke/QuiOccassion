e120f55125cd7282407a23ba10397280626df0e2
Michael Mukolwe <michaelolukaka@gmail.com>
2022-01-27 15:43:16 +0300

    init project & setup landing page

---
+++ auth/register.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <meta content="" name="description" />
+    <meta content="" name="keywords" />
+
+    <title>Qui Occasions - Register</title>
+
+    <!-- Icons -->
+    <link
+      href="https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css"
+      rel="stylesheet"
+    />
+
+    <!-- Styles -->
+    <!-- Bootsrap -->
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+    />
+
+    <!-- Custom -->
+    <link href="/assets/css/main.css" rel="stylesheet" />
+  </head>
+  <body>
+    <section class="landing-page auth-page">
+      <div class="left-page">
+        <div class="logo-wrapper">
+          <img src="/assets/img/logo.png" alt="Main Logo" />
+        </div>
+      </div>
+      <div class="right-page">
+      <nav class="navbar navbar-expand-lg navbar-light bg-transparent" style="border-bottom: 1px solid #a7a4a4c4; height: 100px">
+        <div class="container-fluid">
+            <button
+                class="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarNav"
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div
+                class="collapse navbar-collapse justify-content-end"
+                id="navbarNav"
+            >
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/about.php">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/events.php">Events</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="btn btn-primary" href="/auth/login.php">Getting Started</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+      </nav>
+      </div>
+    </section>
+
+    <!-- Template Main JS File -->
+    <script src="/assets/js/main.js"></script>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
+  </body>
+</html>
